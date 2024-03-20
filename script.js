c09d075278245878aa74fda427bec9c848a66917
Nagendra-Sure <nagendrasure111@gmail.com>
2024-03-20 14:24:22 +0530

    Final Code

--- script.js
+++ script.js
@@ -10,10 +10,10 @@
 // For input feilds we will be using .value
 // document.querySelector('.guess').value = 23;
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highScore = 0;
 
 //  for all the clickings we need to use event listener
 document.querySelector('.check').addEventListener('click', function () {
@@ -23,6 +23,16 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Please enter number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉Correct Number !';
+    document.querySelector('.number').textContent = secretNumber;
+    // When player wins the gave it changes tha background color.
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high !';
@@ -45,3 +55,16 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 // The above code is for clicking the button and in its input it logs some value to console
+
+// Now we are writing the code to reset the entire game
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start Guessing... ';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
